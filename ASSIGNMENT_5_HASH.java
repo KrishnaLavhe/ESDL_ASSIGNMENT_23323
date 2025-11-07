f1cbc08c9ae120f305aa3e81e2e7674729d55a4b
KrishnaLavhe <krishnaaa.lavhe@gmail.com>
2025-11-07 09:34:08 +0530

    Create ASSIGNMENT_5_HASH.java

---
+++ ASSIGNMENT_5_HASH.java
@@ -0,0 +1,17 @@
+
+//Name-Aniket Chandrakant Joshi
+//Rollno-23313
+import java.util.HashMap;
+
+public class Assign5_Hash {
+    public static void main(String[] args) {
+        HashMap<String, String> StudentDetail = new HashMap<String, String>();
+        StudentDetail.put("Aniket", "23313");
+        StudentDetail.put("Devang", "23305");
+        StudentDetail.put("Om", "23362");
+
+        System.out.println(StudentDetail);
+
+    }
+
+}
